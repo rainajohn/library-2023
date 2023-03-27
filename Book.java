5c6264024851762b94cb477c4c5ccac451331a06
rainajohn <103225495+rainajohn@users.noreply.github.com>
2023-03-28 09:11:21 +1300

    Added fields for book class

---
+++ Book.java
@@ -0,0 +1,36 @@
+
+/**
+ * Support class of book 
+ * A book contains an id, name, author, quatity, image
+ *
+ * @Raina
+ * @28/03/23
+ */
+public class Book
+{
+    // instance variables
+    private int id;
+    private String name;
+    private String author;
+    private int quantity;
+    private String image;
+    
+
+    /**
+     * Constructor for objects of class Book
+     */
+    public Book(int id, String nm, String auth, int qty, String img)
+    {
+        // initialise instance variables
+        this.id = id;
+        this.name = nm;
+        this.author = auth;
+        this.quantity = qty;
+        this.image = img; 
+    }
+
+    /**
+     * 
+     */
+    
+}
---
+++ Books.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;  // import the hashmap class
+import ecs100.*;
+/**
+ * Holds collection of books in hashmap
+ * Allows user to add, find, print all
+ *
+ * @Raina
+ * @28/03/23
+ */
+public class Books
+{
+    // instance variables - replace the example below with your own
+    private HashMap<String, String> Books;
+
+    /**
+     * Constructor for objects of class Books
+     */
+    public Books()
+    {
+        // initialise instance variables
+        
+    }
+
+    
+}
